d1dc87ecd5d11bb3600e71adfeb8c7e586410b19
lucosmo <lukasz.kosmaczewski@gmail.com>
2021-11-26 20:54:50 +0000

    if arguments are missing, pomodoro stops working; added 3rd argument - number of loops

--- pomodoro.php
+++ pomodoro.php
@@ -2,22 +2,23 @@
 require 'work.php';
 require 'relax.php';
 
-var_dump($argv);
+#var_dump($argv);
 $work = null;
 $rest = null;
+$loops = null;
 if (count($argv)!=3) {
-  echo "missing arguments!\n php pomodoro.php work_time break_time\n work_time, break_time - time in minutes";
+  exit(" missing arguments!\n php pomodoro.php work_time break_time number_repeats\n work_time, break_time - time in minutes\n number_repeats - (int) how many loops will pomodoro do\n");
 }
 else {
   $work = (float)$argv[1];
   $rest = (float)$argv[2];
+  $loops = (int)$argv[3];
 }
 
 $start = microtime(true);
-#$work=10;
-
 $w = new Work($work);
 $w->countdown();
+
 $start = microtime(true);
 $b = new Relax($rest);
 $b->countdown();
