6b94d5ffeeb52a42999ef075b0d1b9f27aa5d35f
lucosmo <lukasz.kosmaczewski@gmail.com>
2021-11-27 21:53:45 +0000

    Symfony PHP coding style -> @var, @param, @return in timer.php

--- timer.php
+++ timer.php
@@ -9,20 +9,28 @@ class Timer{
 
   /**
    * @var float $start Start point for countown process
-  */
-
+   */
   private $start = 0;
 
   /**
    * @var float $length Describes how long does the countdown phase takes
-  */
-
+   */
   private $length  = 0;
 
+  /**
+   * @param float $length How long is countdown in minutes
+   */
   public function __construct(float $length){
     $this->length = $this->minutesToSeconds($length);
   }
 
+  /**
+   * Counting from the momemnt of invoking the function set with counstructor amount of time, sending system signal at the end
+   *
+   * @param string $text Text which will be printed at the end of the countdown
+   *
+   * @return void
+   */
   public function countdown(string $text){
     $this->start = microtime(true);
     while($i = microtime(true) < $this->start + $this->length){
@@ -33,11 +41,23 @@ class Timer{
     return;
   }
 
+  /**
+   * Invoking system signal
+   *
+   * @return void
+   */
   private function beep(){
     fprintf ( STDOUT, "%s", "\x07" );
     return;
   }
 
+  /**
+   * Changes minutes (float number) into seconds
+   *
+   * @param float $length Number of minutes
+   *
+   * @return float
+   */
   private function minutesToSeconds(float $length){
     return $length*60.0;
   }
