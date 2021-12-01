047602a82abb42da4f536101999f4d7dd3e2a9bc
lucosmo <lukasz.kosmaczewski@gmail.com>
2021-12-01 23:17:07 +0000

    ProgressBar display() method done

--- src/progress_b.php
+++ src/progress_b.php
@@ -5,5 +5,15 @@ namespace Pomodoro;
  * Class ProgressBar provides progress bar functionality for Pomodoro tool.
  */
 class ProgressBar {
+  $current = null;
+  $total = null;
 
+  function __construct($total, $current=0) {
+    $this->total = $total;
+    $this->current = $current;
+  }
+
+  function display($done) {
+    echo round($done/$total*100)."%\r";
+  }
 }
